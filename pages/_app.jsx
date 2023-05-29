66c463bd49e8529028abf19dbc650d41425a38ff
Thomas Hong <thomas_hong23@milton.edu>
2023-05-29 15:57:22 -0400

    more vercel things

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,23 +3,7 @@ import { auth } from '../firebase';
 import firebase from 'firebase/compat/app';
 
 export default function App({ Component, pageProps }) {
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      if (user) {
-        setUser(user);
-        setLoading(false);
-      } else {
-        setUser(null);
-        setLoading(false);
-      }
-    });
-
-    return () => unsubscribe();
-  }, []);
 
   if (loading) {
     return <div>Loading...</div>;
