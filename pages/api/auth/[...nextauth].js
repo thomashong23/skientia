79011717ecd044dfa722238373e7f65001cfcb56
Thomas Hong <thomas_hong23@milton.edu>
2023-06-03 14:06:11 -0400

    setting constants equal to the process variables

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,13 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-console.log('GOOGLE_CLIENT_ID:', process.env.GOOGLE_CLIENT_ID);
-console.log('NEXT AUTH SECRET:', process.env.NEXT_PUBLIC_SECRET);
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const NEXT_PUBLIC_SECRET = process.env.NEXT_PUBLIC_SECRET;
+
+// Log environment variable values
+console.log('GOOGLE_CLIENT_ID:', GOOGLE_CLIENT_ID);
+console.log('GOOGLE_CLIENT_SECRET:', GOOGLE_CLIENT_SECRET);
+console.log('NEXT_PUBLIC_SECRET:', NEXT_PUBLIC_SECRET);
 export const authOptions = {
   providers: [
     GoogleProvider({
@@ -13,6 +19,6 @@ export const authOptions = {
   session: {
     strategy: 'jwt',
   },
-  secret: process.env.NEXT_PUBLIC_SECRET,
+  secret: NEXT_PUBLIC_SECRET,
 };
 export default NextAuth(authOptions);
