28fc6ffdff346a82cb57b41ea8bb4494d571eec3
Thomas Hong <thomas_hong23@milton.edu>
2023-06-04 00:02:05 -0400

    async

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,24 +1,29 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-const NEXT_PUBLIC_SECRET = process.env.NEXT_PUBLIC_SECRET;
 
-// Log environment variable values
-console.log('GOOGLE_CLIENT_ID:', GOOGLE_CLIENT_ID);
-console.log('GOOGLE_CLIENT_SECRET:', GOOGLE_CLIENT_SECRET);
-console.log('NEXT_PUBLIC_SECRET:', NEXT_PUBLIC_SECRET);
-export const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
-      redirect_uri: "https://skientia.vercel.app/api/auth/callback/google",
-    }),
-  ],
-  session: {
-    strategy: 'jwt',
-  },
-  secret: NEXT_PUBLIC_SECRET,
-};
-export default NextAuth(authOptions);
+export default async function Auth(req, res) {
+  const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+  const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+  const NEXT_PUBLIC_SECRET = process.env.NEXT_PUBLIC_SECRET;
+
+  // Log environment variable values
+  console.log('GOOGLE_CLIENT_ID:', GOOGLE_CLIENT_ID);
+  console.log('GOOGLE_CLIENT_SECRET:', GOOGLE_CLIENT_SECRET);
+  console.log('NEXT_PUBLIC_SECRET:', NEXT_PUBLIC_SECRET);
+
+  const authOptions = {
+    providers: [
+      GoogleProvider({
+        clientId: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
+        redirect_uri: "https://skientia.vercel.app/api/auth/callback/google",
+      }),
+    ],
+    session: {
+      strategy: 'jwt',
+    },
+    secret: NEXT_PUBLIC_SECRET,
+  };
+
+  await NextAuth(req, res, authOptions);
+}
