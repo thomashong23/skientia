b2abfdc8a2552875ede1401cd6d906a09c756e0a
Thomas Hong <thomas_hong23@milton.edu>
2023-06-07 01:46:45 -0400

    rip

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,26 +1,20 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 const NEXT_PUBLIC_SECRET = process.env.NEXT_PUBLIC_SECRET;
 
-// Log environment variable values
-console.log('GOOGLE_CLIENT_ID:', GOOGLE_CLIENT_ID);
-console.log('GOOGLE_CLIENT_SECRET:', GOOGLE_CLIENT_SECRET);
-console.log('NEXT_PUBLIC_SECRET:', NEXT_PUBLIC_SECRET);
-export const authOptions = {
+const authOptions = {
   providers: [
     GoogleProvider({
       clientId: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-    }
-    )
-
+    }),
   ],
-
   session: {
     strategy: 'jwt',
   },
-
 };
-export default NextAuth(authOptions);
+
+export default (req, res) => NextAuth(req, res, authOptions);
